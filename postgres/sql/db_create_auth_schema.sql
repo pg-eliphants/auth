70acabda654fd9450c378ccace64a2ae63f1a4db
jacobbogers <info@mail.jacob-bogers.com>
2023-07-03 20:06:40 +0200

    created schema (postgres) for storage of user and user credentials/properties

--- postgres/sql/db_create_auth_schema.sql
+++ postgres/sql/db_create_auth_schema.sql
@@ -1,104 +1,74 @@
 -- fail on the first error
 \set ON_ERROR_STOP on
 
-CREATE ROLE authenticator WITH
+CREATE ROLE authenticator WITH NOINHERIT;
     -- default NOSUPERUSER
     -- default NOCREATEDB
     -- default NOCREATEROLE
-    NOINHERITS
     -- default NOLOGIN
     -- default NOREPLICATION 
     -- default NOBYPASSRLS
     -- default CONNECTION LIMIT -1 -- -1 = no limit
 
-CREATE SCHEMA auth IF NOT EXISTS AUTHORIZATION authenticator;
+CREATE SCHEMA auth AUTHORIZATION authenticator;
 
 ALTER ROLE authenticator SET search_path = auth;
 
 -- directory '/data/authentication' must exist and user postgres must be sole owner
-CREATE TABLESPACE auth_storage OWNER auth LOCATION '/data/authentication';
+CREATE TABLESPACE auth_storage OWNER authenticator LOCATION '/data/authentication';
 
 SET default_tablespace = auth_storage;
 
-DROP SEQUENCE IF EXISTS seq_user_id RESTRICT;
-CREATE SEQUENCE seq_user_id CACHE 500;
-  
+CREATE SEQUENCE auth.seq_user_id AS integer CACHE 500;
+
 
 CREATE TABLE auth.user (
-    id VARCHAR(120), --pk
-    cr_ts timestamp,
-    name VARCHAR(30) NOT NULL,  --nick of user, other user attributes in "user_props" table
-    email VARCHAR(120) NOT NULL, --unique
+    id integer NOT NULL DEFAULT nextval('auth.seq_user_id'),
+    cr_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX user_name_udx ON auth.user(UPPER(name));
-CREATE UNIQUE INDEX user_email_udx ON auth.user(UPPER(email));
---
+
+ALTER SEQUENCE auth.seq_user_id OWNED BY auth.user.id;
+
 CREATE TABLE auth.user_props (
-    fk_user_id VARCHAR(120),
-    prop_name VARCHAR(30),
-    prop_value VARCHAR(256) NOT NULL DEFAULT '',
-    invisible CHAR(1) NOT NULL DEFAULT 'F',
-    CONSTRAINT user_props_user_fk
+    fk_user_id integer NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    value VARCHAR(256) NOT NULL,
+    tombstone BOOLEAN DEFAULT FALSE,
+    cr_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT user_props_fk
+        FOREIGN KEY (fk_user_id) REFERENCES auth.user(id) ON DELETE CASCADE
+);
+-- do not schema prefix for indexes
+CREATE INDEX user_props_idx ON auth.user_props(UPPER(type), fk_user_id);
+CREATE INDEX user_props_idx2 ON auth.user_props(fk_user_id, UPPER(type));
+
+
+--postgres=# select * from pg_indexes view where tablespace = 'auth_storage';
+-- schemaname | tablename  |    indexname    |  tablespace  |                                           indexdef                                            
+--------------+------------+-----------------+--------------+-----------------------------------------------------------------------------------------------
+-- auth       | user       | pk_user         | auth_storage | CREATE UNIQUE INDEX pk_user ON auth."user" USING btree (id)
+-- auth       | user_props | user_props_idx  | auth_storage | CREATE INDEX user_props_idx ON auth.user_props USING btree (upper((type)::text), fk_user_id)
+-- auth       | user_props | user_props_idx2 | auth_storage | CREATE INDEX user_props_idx2 ON auth.user_props USING btree (fk_user_id, upper((type)::text))
+--
+
+CREATE TABLE auth.user_props_bin (
+    fk_user_id integer NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    value bytea NOT NULL,
+    CONSTRAINT user_props_bin_fk
         FOREIGN KEY (fk_user_id) REFERENCES auth.user(id) ON DELETE CASCADE
 );
 
-CREATE TABLE auth.user_props (
-    fk_user_id VARCHAR(120), 
-    cr_date timestamp, -- 8 bytes
-    prop_name VARCHAR(30),
-    prop_value VARCHAR(256) NOT NULL DEFAULT '',
-    invisible CHAR(1) NOT NULL DEFAULT 'F',
-    CONSTRAINT user_props_user_fk
-        FOREIGN KEY (fk_user_id) REFERENCES auth.user(id) ON DELETE CASCADE
-);
+CREATE INDEX user_props_bin_idx ON auth.user_props_bin(UPPER(type), fk_user_id);
+CREATE INDEX user_props_bin_idx2 ON auth.user_props_bin(fk_user_id, UPPER(type));
 
+-- postgres=# select * from pg_indexes view where tablespace = 'auth_storage';
+-- schemaname |   tablename    |      indexname      |  tablespace  |                                               indexdef                                                
+--------------+----------------+---------------------+--------------+-------------------------------------------------------------------------------------------------------
+-- auth       | user           | pk_user             | auth_storage | CREATE UNIQUE INDEX pk_user ON auth."user" USING btree (id)
+-- auth       | user_props     | user_props_idx      | auth_storage | CREATE INDEX user_props_idx ON auth.user_props USING btree (upper((type)::text), fk_user_id)
+-- auth       | user_props     | user_props_idx2     | auth_storage | CREATE INDEX user_props_idx2 ON auth.user_props USING btree (fk_user_id, upper((type)::text))
+-- auth       | user_props_bin | user_props_bin_idx  | auth_storage | CREATE INDEX user_props_bin_idx ON auth.user_props_bin USING btree (upper((type)::text), fk_user_id)
+-- auth       | user_props_bin | user_props_bin_idx2 | auth_storage | CREATE INDEX user_props_bin_idx2 ON auth.user_props_bin USING btree (fk_user_id, upper((type)::text))
 
-CREATE UNIQUE INDEX user_props_user_udx ON auth.user_props(fk_user_id, UPPER(prop_name));
-CREATE INDEX user_props_user_name_idx ON auth.user_props(UPPER(prop_name), fk_user_id);
---
-CREATE TABLE auth.session_cookies_template ( -- insert a dummy '0' value
-    id VARCHAR(120),
-    template_name VARCHAR(30) NOT NULL,
-    cookie_name VARCHAR(30),
-    path VARCHAR(128) NOT NULL DEFAULT '/',
-    max_age BIGINT, -- in ms
-    http_only CHAR(1) NOT NULL DEFAULT 'T',
-    secure CHAR(1) NOT NULL DEFAULT 'T',
-    domain VARCHAR(128),
-    same_site CHAR(1) NOT NULL DEFAULT 'T',
-    rolling CHAR(1) NTO NULL DEFAULT 'T',
-    CONSTRAINT session_cookies_template_pk PRIMARY KEY (id)
-);
-
-CREATE UNIQUE INDEX sct_pk ON session_cookies_template(id);
-CREATE UNIQUE INDEX sct_uix ON session_cookies_template(UPPER(template_name));
---
-CREATE TABLE issued_user_tokens (
-    id VARCHAR(64),                          -- token id
-    fk_user_id VARCHAR(120),                       -- user id
-    purpose CHAR(4) NOT NULL,                         -- CHAR-mnemonic for the purpose of issuing
-    ip_addr VARCHAR2(100),                            -- ip@port of the user agent when this token was issued
-    timestamp_issued BIGINT NOT NULL,        -- time of issuance
-    timestamp_revoked BIGINT DEFAULT NULL,   -- if revoked, this is when!...
-    revoke_reason CHAR(2) NOT NULL DEFAULT 'EX',                   -- if revoked, this is why! (MNEMONIC)
-    timestamp_expire BIGINT NOT NULL,        -- timestamp when this token expires
-    fk_cookie_template_id BIGINT DEFAULT 0,  -- more info if this token is a cookie-token, default 0 is a dud template
-    CONSTRAINT pk_issued_token PRIMARY KEY (id),
-    CONSTRAINT fk_issued_token_user FOREIGN KEY (fk_user_id) REFERENCES auth.user(id) ON DELETE CASCADE,
-    CONSTRAINT fk_session_cookie_template FOREIGN KEY (fk_cookie_template_id) REFERENCES auth.session_cookies_template(id) ON DELETE CASCADE
-);
-CREATE INDEX issued_token_udx ON issued_user_tokens(id);
-CREATE INDEX issued_token_user_idx ON issued_user_tokens(fk_user_id);
-CREATE INDEX issued_tokens_expired_keys ON issued_user_tokens(timestamp_expire);
-CREATE INDEX issued_tokens_revoked ON issued_user_tokens(timestamp_revoked);
---
-CREATE TABLE session_props (
-    fk_token_id VARCHAR(64),
-    session_prop_name VARCHAR(30),
-    session_prop_value VARCHAR(120),
-    invisible CHAR(1) NOT NULL DEFAULT 'F',
-    CONSTRAINT pk_session_props PRIMARY KEY (fk_token_id, UPPER(session_prop_name)),
-    CONSTRAINT fk_token_id FOREIGN KEY (fk_token_id) REFERENCES auth.issued_user_tokens(id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX session_props_idx ON auth.session_props(fk_token_id, UPPER(session_prop_name));
--- postgres/sql/db_create_db.sql
+++ postgres/sql/db_create_db.sql
@@ -1,23 +1,4 @@
-CREATE USER bookbarter WITH
-    ENCRYPTED PASSWORD 'bookbarter' -- choose better password))
-    LOGIN
-    NOSUPERUSER
-    INHERIT
-    NOCREATEDB
-    NOCREATEROLE
-    NOREPLICATION;
-
-CREATE DATABASE bookbarter -- yes has same name as user
+CREATE DATABASE auth_db -- yes has same name as user
     WITH
-    OWNER = postgres -- pick a user with create db rights
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    TEMPLATE = template0
     CONNECTION LIMIT = -1;
 
-COMMENT ON DATABASE bookbarter
-    IS 'The book barter app';
-
-GRANT ALL ON DATABASE bookbarter TO bookbarter;
