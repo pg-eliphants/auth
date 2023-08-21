214c0fe82ca1d41e4bd4abf993b7802fcb383bbb
Jacob Bogers <jkfbogers@gmail.com>
2023-08-21 23:18:39 +0200

    got linewire prepare working

--- runme.ts
+++ runme.ts
@@ -14,30 +14,46 @@ function login_nossl_nopassword() {
   });
 }
 
+const query1 = {
+  text: "PREPARE fooplanx (varchar) AS select oid, typname from pg_type where typname = $1",
+};
+
+const query2 = {
+  text: "select * from pg_prepared_statements",
+};
+
+function delay(ts_in_sec: number) {
+  const ts_in_ms = Math.trunc(ts_in_sec * 1e3);
+  return new Promise((resolve) => setTimeout(resolve, ts_in_ms));
+}
+
 async function testConnection(connection: () => PG.Client) {
   // postgres[ql]://[username[:password]@][host[:port],]/database[?parameter_list]
   const cl = connection();
   await cl.connect();
   {
     const rows = await cl.query({
-      text: "PREPARE fooplanx (varchar) AS select oid, typname from pg_type where typname = $1",
+      name: "foobar",
+      //portal: "foobar",
+      text: "select oid, typname from pg_type where typname = $1",
+      values: ["bool"],
     });
     console.log("prepare plan rows: [%o]", rows);
   }
-  await new Promise((resolve) => setTimeout(resolve, 3e3));
+  await delay(3);
   {
     const rows = await cl.query({
       text: "select * from pg_prepared_statements",
     });
     console.log("query pg_prepared_statements: [%o]", rows);
   }
-  await new Promise((resolve) => setTimeout(resolve, 3e3));
+  /*await new Promise((resolve) => setTimeout(resolve, 3e3));
   {
     const rows = await cl.query({
-      text: "execute fooplanx('bool')",
+      text: "execute foobar('bool')",
     });
     console.log("query result: [%o]]", rows);
-  }
+  }*/
 }
 
 testConnection(login_nossl_nopassword);
